d88a71234a383b2fb6c3b9ec111caeca6f353601
yogiyiorgos <yiorgosdev@gmail.com>
2023-09-23 23:48:11 +0200

    GS ProductScreen Sep23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
-    <h1>Welcome to GeoShop</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
