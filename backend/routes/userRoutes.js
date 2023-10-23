254dd2d5f5d6ed4ac8c51c25c0e2ababe3459ec4
yogiyiorgos <yiorgosdev@gmail.com>
2023-10-23 19:34:02 +0200

    GS PPInterg Oct 23

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,7 +16,7 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/auth', authUser)
 router.post('/logout', logoutUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/profile').get(getUserProfile).put(protect, updateUserProfile)
 router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 export default router
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { Provider } from 'react-redux'
 import store from './store'
 // import 'bootstrap/dist/css/bootstrap.min.css'
@@ -21,6 +22,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 import PrivateRoute from './components/PrivateRoute'
 
 import reportWebVitals from './reportWebVitals'
@@ -37,17 +39,19 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
       </Route>
     </Route>
   )
 )
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 )
